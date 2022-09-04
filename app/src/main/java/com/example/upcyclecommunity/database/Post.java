6eb44b3ffe452d6afa671adf66275b9c5cd2ff5e
DongGi-create <84391428+DongGi-create@users.noreply.github.com>
2022-09-04 23:10:38 +0900

    사진 업로드

---
+++ app/src/main/java/com/example/upcyclecommunity/database/Post.java
@@ -0,0 +1,30 @@
+package com.example.upcyclecommunity.database;
+
+import java.util.ArrayList;
+//regrdfvsfdsvbgndnhdnhnnr
+public class Post {
+    String title;
+    ArrayList<String> contents;
+    String kfjlk;
+
+    public Post(String title, ArrayList<String> contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public ArrayList<String> getContents() {
+        return contents;
+    }
+
+    public void setContents(ArrayList<String> contents) {
+        this.contents = contents;
+    }
+}
